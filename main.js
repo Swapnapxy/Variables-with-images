21946de36e19e66575228fefa0afbea27ffc8d70
Swapnapxy <146064964+Swapnapxy@users.noreply.github.com>
2023-09-26 16:01:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var score=0;
+function increase(){
+    score=score+5;
+    document.getElementById("marks").textContent=score;
+    
+}
+function decrease(){
+    score=score-5;
+    document.getElementById("marks").textContent=score;
+    
+}
